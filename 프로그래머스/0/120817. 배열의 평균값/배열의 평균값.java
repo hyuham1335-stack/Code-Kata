17bdc7a5ced838a0382c4c1825ba514e2733e76d
hyu1335 <hyuham1335@gmail.com>
2026-01-21 09:31:59 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum =0 ;
+        for(int a : numbers) {
+            sum+=a;
+        }
+        answer = sum/numbers.length;
+        return answer;
+    }
+}
