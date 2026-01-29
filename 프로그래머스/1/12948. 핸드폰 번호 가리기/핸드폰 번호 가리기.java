e5993f464ffdbc438a6d70094e5050d020105add
hyu1335 <hyuham1335@gmail.com>
2026-01-29 09:13:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String phone_number) {
+        
+        char[] array = phone_number.toCharArray();
+        
+        for(int i=0; i<array.length-4;i++){
+            array[i] = '*';
+        }
+        
+        String answer = new String(array);
+        
+        return answer;
+    }
+}
