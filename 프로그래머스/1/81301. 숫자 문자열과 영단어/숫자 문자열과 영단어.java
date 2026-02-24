b8f619e23d30c57ea554c49014cf95754431a049
hyu1335 <hyuham1335@gmail.com>
2026-02-24 09:37:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.61 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,63 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String answerString = "";
         
-        for(int i=0;i<s.length();i++){
-                    
-            char c = s.charAt(i);
-            
-            switch(c){
-                    case 'z' -> {
-                        answerString += "0";
-                        i+=3;
-                    }
-                    case 'o' -> {
-                        answerString += "1";
-                        i+=2;
-                    }
-                    case 't' -> {
-                        if(s.charAt(i+1) == 'w') {
-                            answerString += "2";
-                            i+=2;
-                        } else {
-                            answerString += "3";
-                            i+=4;
-                        }
-                    }
-                    case 'f' -> {
-                        if(s.charAt(i+1) == 'o') {
-                            answerString += "4";
-                            i+=3;
-                        } else {
-                            answerString += "5";
-                            i+=3;
-                        }
-                    }
-                    case 's' -> {
-                        if(s.charAt(i+1) == 'i') {
-                            answerString += "6";
-                            i+=2;
-                        } else {
-                            answerString += "7";
-                            i+=4;
-                        }
-                    }
-                    case 'e' -> {
-                        answerString += "8";
-                        i+=4;
-                    }
-                    case 'n' -> {
-                        answerString += "9";
-                        i+=3;
-                    }
-                    default -> answerString+=s.substring(i,i+1);
-
-            }
-            
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i=0 ; i< arr.length ; i++){
+            s = s.replaceAll(arr[i], Integer.toString(i));
         }
         
-        answer = Integer.parseInt(answerString);
+        answer = Integer.parseInt(s);
+        
         return answer;
     }
 }
